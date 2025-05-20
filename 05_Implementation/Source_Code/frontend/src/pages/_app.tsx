9915a820ffe2cca54b021ca803775990e5845a50
Mahmoudramadan21 <mahmoud.fci25@gmail.com>
2025-05-20 13:00:00 +0300

    Update _app and feed page

--- 05_Implementation/Source_Code/frontend/src/pages/_app.tsx
+++ 05_Implementation/Source_Code/frontend/src/pages/_app.tsx
@@ -1,18 +1,6 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
-import HeaderSection from '../sections/HeaderSection';
 import '../styles/globals.css';
-import StoriesSection from '@/sections/StoriesSection';
-import PostCard from '@/components/PostCard';
-import FollowRequests from '@/components/FollowRequests';
-import UserMenu from '@/components/UserMenu';
-import CreatePost from '@/components/CreatePost';
-import Notifications from '@/components/Notifications';
-import StoriesList from '@/components/StoriesList';
-import StoryViewer from '@/components/StoryViewer';
-import CreateStories from '@/components/CreateStories';
-import Loading from '@/components/Loading';
-import UserBanner from '@/components/UserBanner';
 
 // Main App component for Next.js
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
