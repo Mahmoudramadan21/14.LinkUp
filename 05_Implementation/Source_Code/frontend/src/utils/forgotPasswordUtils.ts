46d9589b71fac0f7041bacf2be3710aaa9b060c1
Mahmoudramadan21 <mahmoud.fci25@gmail.com>
2025-09-29 21:00:00 +0000

    Remove forgotPasswordUtils.ts due to new auth utilities

--- 05_Implementation/Source_Code/frontend/src/utils/forgotPasswordUtils.ts
+++
@@ -1,7 +0,0 @@
-export const getCsrfToken = (): string => {
-  return document.querySelector('meta[name="csrf-token"]')?.getAttribute('content') || '';
-};
-
-export const storeResetEmail = (email: string): void => {
-  localStorage.setItem('resetEmail', email);
-};
